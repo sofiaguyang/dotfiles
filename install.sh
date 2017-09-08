21f990fc1d742242bf82ccfbe725a31bc841c76d
Sofia Ang <sofiaguyang@users.noreply.github.com>
2017-09-08 11:43:40 +0800

    Customize key repeat

--- install.sh
+++ install.sh
@@ -501,12 +501,12 @@ defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true;ok
 running "Use F1, F2, etc as standard function keys"
 defaults write -g com.apple.keyboard.fnState -boolean true; ok
 
-#running "Disable press-and-hold for keys in favor of key repeat"
-#defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false;ok
+running "Disable press-and-hold for keys in favor of key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false;ok
 
 running "Set a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10;ok
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 12;ok
 
 running "Set language and text formats (english/US)"
 defaults write NSGlobalDomain AppleLanguages -array "en"

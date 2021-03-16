214ed5bab6b9edd579f637d3bd60471f6a1bfc20
Sofia Guy Ang <sofiaguyang@users.noreply.github.com>
2021-03-16 20:32:32 +1100

    Update because brew cask no longer exists

--- lib_sh/requirers.sh
+++ lib_sh/requirers.sh
@@ -8,11 +8,11 @@
 # source ./echos.sh
 
 function require_cask() {
-    running "brew cask $1"
-    brew cask list $1 > /dev/null 2>&1 | true
+    running "brew $1"
+    brew list $1 > /dev/null 2>&1 | true
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
-        action "brew cask install $1 $2"
-        brew cask install $1
+        action "brew install $1 $2"
+        brew install $1
         if [[ $? != 0 ]]; then
             error "failed to install $1! aborting..."
             # exit -1
